a493f15c8864fa526423df129d52454f08c17bf4
SobhanMridha <sobhanmridha786.com>
2025-07-26 16:54:59 +0600

    add login and register features with session

--- app.py
+++ app.py
@@ -39,38 +39,87 @@ def close_db(exception):
 
 @app.route('/')
 def index():
-    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=en-US&page=1"
-    res = requests.get(url)
-    data = res.json()
-   
-    return render_template("index.html", data=data)
+    title = request.args.get('title', '').strip()
+    selected_category = request.args.get('category')
+    data = None
+
+    if title:
+        url = 'https://api.themoviedb.org/3/search/movie'
+        params = {
+            'api_key': API_KEY,
+            'language': 'en-US',
+            'query': title,
+            'page': 1,
+            'include_adult': False
+        }
+        res = requests.get(url, params=params)
+        data = res.json()
+
+    elif selected_category:
+        url = f"https://api.themoviedb.org/3/movie/{selected_category}"
+        params = {
+            'api_key': API_KEY,
+            'language': 'en-US',
+            'page': 1
+        }
+        res = requests.get(url, params=params)
+        data = res.json()
+    else:
+        url = "https://api.themoviedb.org/3/movie/now_playing"
+        params = {
+            'api_key': API_KEY,
+            'language': 'en-US',
+            'page': 1
+        }
+        res = requests.get(url, params=params)
+        data = res.json()
+
+    return render_template("index.html", data=data, selected_category=selected_category)
+
 
 @app.route("/watchlist", methods=["GET", "POST"])
 def watchlist():
-  
+    if not session.get("user_id"):
+        flash("You must be logged in to view this page.", "error")
+        return redirect("/login")
+
     return render_template("watchlist.html")
 
 @app.route("/watched", methods=["GET", "POST"])
 def watched():
-   
+    if not session.get("user_id"):
+        flash("You must be logged in to view this page.", "error")
+        return redirect("/login")
 
     return render_template("watched.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    session.clear()
-
+    
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        if username and password:
+
+        if not username or not password:
+            flash("Please enter both username and password.", "error")
+            return redirect("/login")
+        
+        db = get_db()
+        user = db.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone()
+        print(user)
+        if user and check_password_hash(user["password_hash"], password):
+            
+            session["user_id"] = user["id"]
             flash("Logged in successfully!", "success")
             return redirect("/")
         else:
-            flash("Please enter both username and password.", "error")
+            flash("Invalid username or password.", "error")
+            return redirect("/login")
+    
 
     return render_template("login.html")
+
 
 
 @app.route("/logout")
@@ -86,15 +135,25 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
-        print(username, password, confirm_password)
+    
         if password != confirm_password:
             flash("Passwords do not match.", "error")
             return redirect("/register")
         if username and password:
+            hash = generate_password_hash(password)
+            try:
+                db = get_db()
+                db.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", (username, hash))
+                db.commit()
+            except:
+                flash("Username already exists.", "error")
+                return redirect("/register")
+
             flash("Registered successfully!", "success")
-            return redirect("/")
+            return redirect("/login")
         else:
             flash("Please enter both username and password.", "error")
+            return redirect("/register")
 
     return render_template("register.html")
 
